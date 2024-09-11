f92b5884aa9d0289e6f33511cab30f59bfefc879
luizmatoso <luizhmatoso1340@gmail.com>
2024-09-11 18:43:43 -0300

    Initial commit

---
+++ src/main/java/com/luiz_matoso/e_commerce_payment_system/ECommercePaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luiz_matoso.e_commerce_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz_matoso/e_commerce_payment_system/ECommercePaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz_matoso.e_commerce_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
