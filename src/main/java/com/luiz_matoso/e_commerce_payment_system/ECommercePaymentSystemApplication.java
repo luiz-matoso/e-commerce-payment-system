4d8038ea8e0dfb0dd973fb09f92e48750c811a3d
sory <luizhmatoso1340@gmail.com>
2024-09-21 13:33:46 -0300

    SecurityConfig

--- src/main/java/com/luiz_matoso/e_commerce_payment_system/ECommercePaymentSystemApplication.java
+++ src/main/java/com/luiz_matoso/e_commerce_payment_system/ECommercePaymentSystemApplication.java
@@ -1,9 +1,12 @@
 package com.luiz_matoso.e_commerce_payment_system;
 
+import com.luiz_matoso.e_commerce_payment_system.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { SecurityConfig.class })
 public class ECommercePaymentSystemApplication {
 
 	public static void main(String[] args) {
